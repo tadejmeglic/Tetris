c1a2865a78bc90b32d5d8676080e6ad7279ee62b
tadejmeglic <tadey2204@gmail.com>
2017-07-25 18:56:22 +0200

    Dodal množico blokov in jih nastavil na sredino v zacetku igre

--- logika.py
+++ logika.py
@@ -1,15 +1,36 @@
+import random
+
+Bloki = [[(-1, 0), (0, 0), (1, 0), (2, 0)], [(-1, 0), (-1, 1), (0, 1), (1, 1)], [(1, 0), (-1, 1),
+(0, 1), (1, 1)], [(0, 0), (0, 1), (1, 0), (1, 1)], [(0, 0), (0, 1), (1, 0), (-1, 1)], [(0, 0), (0,
+1), (1, 1), (-1, 1)], [(0, 0), (-1, 0), (0, 1), (1, 1)]]
 
 class Lik:
-    def __init__(self, tocke):
-        self.tocke = tocke
+	def __init__(self, tocke):
+		self.tocke = tocke
 
-    def __repr__(self):
-        return 'Lik({})'.format(self.tocke)
+	def __repr__(self):
+		return 'Lik({})'.format(self.tocke)
 
-    def premakni(self):
-        s=[]
-        for i in range(len(self.tocke)):
-            x, y = self.tocke[i][0], self.tocke[i][1]
-            y+=1
-            s.append((x, y))
-        return s
+	def premakni(self):
+		s=[]
+		for i in range(len(self.tocke)):
+			x, y = self.tocke[i][0], self.tocke[i][1]
+			y+=1
+			s.append((x, y))
+		return s
+
+
+class Igra:
+	def __init__(self, sirina, dolzina):
+		self.sirina = sirina
+		self.dolzina = dolzina
+		dolocen_lik = random.choice(Bloki)
+		s=[]
+		for i in range(len(dolocen_lik)):
+			x, y = dolocen_lik[i][0], dolocen_lik[i][1]
+			print(x, int(self.sirina))
+			x+= int(self.sirina) // 2
+			s.append((x, y))
+		self.lik = Lik(s)
+
+
