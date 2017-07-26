ac89e391441981a504833178c44ac5767b29e270
tadejmeglic <tadey2204@gmail.com>
2017-07-26 19:13:17 +0200

    Naredil veliko sprememb
    Program zazna, ko blok pade na tla in ga zabeleži na ploščo, podobno ko se dotakne drugega bloka, ki je že na plošči.

--- logika.py
+++ logika.py
@@ -4,14 +4,26 @@ Bloki = [[(-1, 0), (0, 0), (1, 0), (2, 0)], [(-1, 0), (-1, 1), (0, 1), (1, 1)], 
 (0, 1), (1, 1)], [(0, 0), (0, 1), (1, 0), (1, 1)], [(0, 0), (0, 1), (1, 0), (-1, 1)], [(0, 0), (0,
 1), (1, 1), (-1, 1)], [(0, 0), (-1, 0), (0, 1), (1, 1)]]
 
-class Lik:
+class Blok:
 	def __init__(self, tocke):
 		self.tocke = tocke
 
 	def __repr__(self):
-		return 'Lik({})'.format(self.tocke)
+		return 'Blok({})'.format(self.tocke)
 
 	def premakni(self):
+		if Igra.blok_v_tla(a) == False:
+			if Igra.blok_v_blok(a) == False:
+				for i in range(len(self.tocke)):
+					self.tocke[i] = (self.tocke[i][0], self.tocke[i][1] + 1)
+				return self.tocke
+			else:
+				a.povrsina += self.tocke
+				Igra.pojav_bloka(a)
+		else:
+			Igra.blok_v_tla(a)
+
+	def premakni_brez_ovire_test(self):
 		s=[]
 		for i in range(len(self.tocke)):
 			x, y = self.tocke[i][0], self.tocke[i][1]
@@ -21,16 +33,52 @@ class Lik:
 
 
 class Igra:
+
 	def __init__(self, sirina, dolzina):
 		self.sirina = sirina
 		self.dolzina = dolzina
-		dolocen_lik = random.choice(Bloki)
+		self.povrsina = []
+
+	def pojav_bloka(self):
+		dolocen_blok = random.choice(Bloki)
 		s=[]
-		for i in range(len(dolocen_lik)):
-			x, y = dolocen_lik[i][0], dolocen_lik[i][1]
+		# Določen blok spravimo na sredino in na vrh igralne površine
+		for i in range(len(dolocen_blok)):
+			x, y = dolocen_blok[i][0], dolocen_blok[i][1]
 			print(x, int(self.sirina))
 			x+= int(self.sirina) // 2
 			s.append((x, y))
-		self.lik = Lik(s)
+		self.blok = Blok(s)
+
+	def blok_v_steno(self):
+		for x, y in self.blok.tocke:
+			if not (0 <= x < self.sirina):
+				return False
+
+	def blok_v_tla(self):
+		a = 0
+		for x, y in self.blok.tocke:
+			print((x, y))
+			if not (0 <= y < self.dolzina - 1):
+				self.povrsina += (self.blok.tocke)
+				self.pojav_bloka()
+				a = 1
+				break
+			else:
+				a = 0
+		if a == 0:
+			return False
+		else:
+			return True
+
+	def blok_v_blok(self):
+		return not len(self.blok.premakni_brez_ovire_test() + self.povrsina) == len(set(self.blok.premakni_brez_ovire_test() + self.povrsina))
+
+
+
+a = Igra(10, 10)
+a.pojav_bloka()
+
+
 
 
